adb7518f7d13adf2d2a7adce9506f5a943aecaaf
Jadson Ribeiro <contato@jadsonbr.com.br>
2021-09-26 23:23:40 -0300

    add new fields in create public tenant

--- tenant_users/tenants/utils.py
+++ tenant_users/tenants/utils.py
@@ -16,8 +16,55 @@ def get_current_tenant():
     tenant = TenantModel.objects.get(schema_name=current_schema)
     return tenant
 
+def create_public_tenant(domain_url, owner_email, username, paid_until, on_trial, **owner_extra):
+    UserModel = get_user_model()
+    TenantModel = get_tenant_model()
+    public_schema_name = get_public_schema_name()
 
-def create_public_tenant(domain_url, owner_email, **owner_extra):
+    if TenantModel.objects.filter(schema_name=public_schema_name).first():
+        raise ExistsError('Public tenant already exists')
+
+    # Create public tenant user. This user doesn't go through object manager
+    # create_user function because public tenant does not exist yet
+    profile = UserModel.objects.create(
+        email=owner_email,
+        username=username,
+        is_active=True,
+        **owner_extra,
+    )
+    profile.set_unusable_password()
+    profile.save()
+
+    # Create public tenant
+
+    if TENANT_SCHEMAS:
+        public_tenant = TenantModel.objects.create(
+            domain_url=domain_url,
+            schema_name=public_schema_name,
+            name='Public Tenant',
+            owner=profile,
+        )
+    else:
+        public_tenant = TenantModel.objects.create(
+            schema_name=public_schema_name,
+            tenant_name=public_schema_name,
+            slug='public'
+            name='Public Tenant',
+            owner=profile,
+        )
+
+        # Add one or more domains for the tenant
+        # get_tenant_domain_model().objects.create(
+        #     domain=domain_url,
+        #     tenant=public_tenant,
+        #     is_primary=True,
+        # )
+
+    # Add system user to public tenant (no permissions)
+    public_tenant.add_user(profile)
+
+
+def old_create_public_tenant(domain_url, owner_email, **owner_extra):
     UserModel = get_user_model()
     TenantModel = get_tenant_model()
     public_schema_name = get_public_schema_name()
